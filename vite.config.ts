346e4f605c8b7215540e2544646db5ce6a6e68b6
NITHIN_360 <nithinbalakrishnan569@gmail.com>
2026-08-21 11:39:31 +0530

    final-enhance-v1

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // ⬅ replace with YOUR backend port
+        target: "https://jachiandmuchi.com", // ⬅ replace with YOUR backend port
         changeOrigin: true,
       },
     },
